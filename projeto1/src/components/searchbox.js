547358cd9ffae668eba09a42e93bad642f39200c
thaisnat <thais.toscano@ccc.ufcg.edu.br>
2018-10-31 17:39:37 -0300

    voltando

---
+++ projeto1/src/components/searchbox.js
@@ -0,0 +1,10 @@
+import sendBotton from './botton-send';
+import textField from './text-field';
+
+const searchbox = props => {
+    return(
+        <div className="search-box">
+                <sendBotton onClick></sendBotton>
+         </div>
+        );
+};
