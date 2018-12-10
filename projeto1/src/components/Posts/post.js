1e98bb839b63269d3a0910bb55ebc3be492d6071
thaisnat <thais.toscano@ccc.ufcg.edu.br>
2018-12-10 12:26:06 -0300

    começando a integrar a api

--- projeto1/src/components/Posts/post.js
+++ projeto1/src/components/Posts/post.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+
+import './post.css';
 import { Row, Col } from 'reactstrap';
 import PropTypes from "prop-types";
-import './post.css';
+
+import { IconButton } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { Api } from "../../Api";
 import AfterPost from './AfterPost/afterPost';
 
 class Post extends Component {
+
+    constructor(props){
+        super(props);
+        this.deletePost = this.deletePost.bind(this);
+    }
+
+    deletePost = () => {
+        Api.delete('listPost/post/' + this.props.card._id).then(response => {
+            if(this.props.deletePost){
+                this.props.deletePost(this.props.post);
+            }
+        })
+    }
 
     render() {
         const { post } = this.props;
@@ -18,6 +36,9 @@ class Post extends Component {
                     <Col xs="10" style={{ marginLeft: -15 + "px", marginTop: 10 + "px" }}>
                     </Col>
                     {post.text}
+                    <IconButton onClick={this.deletePost} aria-label="Delete">
+                        <DeleteIcon/>
+                    </IconButton>
                 </Row>
                 <AfterPost></AfterPost>
             </div>
